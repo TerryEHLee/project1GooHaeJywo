f63f873f53c9089d93d0ead67166e107944b869e
TerryEHLee <capssemerll@gmail.com>
2023-05-16 10:07:49 +0900

    conflict test

--- app.py
+++ app.py
@@ -7,14 +7,18 @@ from bs4 import BeautifulSoup
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://sparta:test@cluster0.ck1xgjf.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient(
+    'mongodb+srv://sparta:test@cluster0.ck1xgjf.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 app = Flask(__name__)
 
-#index.html로 연결
+# index.html로 연결
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route("/teammate_posting", methods=["POST"])
 def movie_post():
@@ -23,21 +27,20 @@ def movie_post():
     hobby_receive = request.form['hobby_give']
     mbti_receive = request.form['mbti_give']
     image_receive = request.form['image_give']
-    myfavoriteURL_receive = request.form['myfavoriteURL_give']                
-    
+    myfavoriteURL_receive = request.form['myfavoriteURL_give']
 
     doc = {
-            'name': name_receive,
-            'age': age_receive,
-            'hobby': hobby_receive,
-            'mbti': mbti_receive,
-            'image': image_receive,  
-            'myfavoriteURL': myfavoriteURL_receive 
+        'name': name_receive,
+        'age': age_receive,
+        'hobby': hobby_receive,
+        'mbti': mbti_receive,
+        'image': image_receive,
+        'myfavoriteURL': myfavoriteURL_receive
     }
 
     db.teammate.insert_one(doc)
-    return jsonify({'msg':'기록하기 완료!'})
+    return jsonify({'msg': '기록하기 완료!'})
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-             
+    app.run('0.0.0.0', port=5100, debug=True)
