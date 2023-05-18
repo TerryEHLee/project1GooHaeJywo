1d27ecf8be39076a8f4a7a2b8b4cdd574b2addb5
podoDJ <ourstar1345@gmail.com>
2023-05-18 09:45:24 +0900

    comment upated function added

--- app.py
+++ app.py
@@ -3,14 +3,14 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-import certifi
+# import certifi
 import requests
 import random
 from bs4 import BeautifulSoup
 
-ca = certifi.where()
+# ca = certifi.where()
 
-client = MongoClient('mongodb+srv://sparta:test@cluster0.qihykt0.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://sparta:test@cluster0.qihykt0.mongodb.net/?retryWrites=true&w=majority') #, tlsCAFile=ca
 db = client.dbsparta
 app = Flask(__name__)
 
@@ -32,8 +32,8 @@ def teammate_post():
     trailer_receive = request.form['trailer_give']
     image_receive = request.form['image_give']
     
-    #m_id는 mongdodb의 teammate 컬렉션에서 검색된 문서들의 개수 + 1~9999사이의 난수이다.!
-    m_id = len(list(db.teammate.find({})))+random.randrange(1, 9999)
+    #m_id는 1~9999사이의 랜덤한 정수이다.!
+    m_id = random.randrange(1, 9999)
 
     doc = {
 		'name':name_receive,
@@ -59,21 +59,38 @@ def teammate_get():
     all_teammates = list(db.teammate.find({},{'_id':False}))
     return jsonify({'result':all_teammates})
 
+# 응원댓글 POST
 @app.route("/commenter", methods=["POST"])
 def commenter_post():
     nickname_receive = request.form['nickname_give']
     comment_receive = request.form['comment_give']
+    #r_id는 20000~29999사이 랜덤정수
+    r_id = random.randrange(20000, 29999)
     doc = {
             'nickname': nickname_receive,
-            'comment': comment_receive
+            'comment': comment_receive,
+            'r_id':r_id,
     }
     db.commenter.insert_one(doc)
     return jsonify({'msg':'응원하기 완료!!'})
 
+# 응원댓글 LIST
 @app.route("/commenter", methods=["GET"])
 def commenter_get():
     commenter_data = list(db.commenter.find({}, {'_id': False}))
     return jsonify({'result':commenter_data})
 
+# 응원댓글 UPDATE
+@app.route("/commenter/<int:r_id>", methods=["POST"])
+def commenter_put(r_id):
+    comment_receive = request.form['comment_give']
+    replace = request.form.get('replace', '').lower() == 'true'
+
+    if replace:
+        db.commenter.update_one({'r_id': r_id}, {'$set': {'comment': comment_receive}})
+        return jsonify({'msg': '수정 완료!!'})
+    else:
+        return jsonify({'msg': 'Invalid request.'})
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5100, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
